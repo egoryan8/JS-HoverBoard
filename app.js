e5fcc68a1d1a282b559455216bdfcabc8178bf0a
Fogel Egor <fogel_92@bk.ru>
2022-05-24 22:40:53 +0700

    refactor: optimize script

--- app.js
+++ app.js
@@ -6,27 +6,26 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
 
-  square.addEventListener('mouseover', () =>
-  setColor(square));
+  square.addEventListener('mouseover', setColor);
 
-  square.addEventListener('mouseleave', () =>
-  removeColor(square));
+  square.addEventListener('mouseleave', removeColor);
 
   board.append(square);
 };
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target;
   const color = getRandomColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 };
 
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target;
   element.style.backgroundColor = '#1d1d1d';
   element.style.boxShadow = `0 0 2px #000`;
 };
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 };
